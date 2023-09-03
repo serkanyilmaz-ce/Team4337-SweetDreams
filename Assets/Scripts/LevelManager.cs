f6e42f3e3e4a4ff2dfa00b9c76265a4005b9134b
Serkan YILMAZ <serkan_yilmaz@zoho.com>
2023-09-03 18:27:49 +0300

    levelmanager updated. Button function assigned.howtoplay screen modified,beautified

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -5,13 +5,18 @@ using UnityEngine;
 
 public class LevelManager : MonoBehaviour
 {
-    public AudioSource[] aSources;
+    public GameObject _quit_Button;
+    public GameObject _howToPlayButton;
+    private AudioSource[] _aSources;
     public GameObject howToPlayScreen;
 
     void Awake()
     {
         Time.timeScale = 0;
         howToPlayScreen.SetActive(true);
+        _quit_Button.SetActive(false);
+        _howToPlayButton.SetActive(false);
+        _aSources = GetComponents<AudioSource>();
     }
     private void Start()
     {
@@ -20,14 +25,27 @@ public class LevelManager : MonoBehaviour
 
     private IEnumerator PlaySounds()
     {
-        aSources[0].Play();
-        yield return new WaitForSeconds(aSources[0].clip.length);
-        aSources[1].Play();
+        _aSources[0].Play();
+        yield return new WaitForSeconds(_aSources[0].clip.length);
+        _aSources[1].Play();
     }
 
     public void close_button()
     {
         howToPlayScreen.SetActive(false);
+        _quit_Button.SetActive(true);
+        _howToPlayButton.SetActive(true);
         Time.timeScale = 1;
+    }
+    public void HowToPlayButton()
+    {
+        howToPlayScreen.SetActive(true);
+        _quit_Button.SetActive(false);
+        _howToPlayButton.SetActive(false);
+        Time.timeScale = 0;
+    }
+    public void quit_button()
+    {
+        Application.Quit();
     }
 }
